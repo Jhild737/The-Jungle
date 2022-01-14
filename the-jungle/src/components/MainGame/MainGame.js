88a8443b6c5c913e27ce6b58cb2561a6ef2418e9
Jhild737 <46577086+Jhild737@users.noreply.github.com>
2022-01-14 17:39:55 -0500

    initial commit and base project

---
+++ the-jungle/src/components/MainGame/MainGame.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './MainGame.module.css';
+
+const MainGame = () => (
+  <div className={styles.MainGame}>
+    MainGame Component
+  </div>
+);
+
+MainGame.propTypes = {};
+
+MainGame.defaultProps = {};
+
+export default MainGame;
